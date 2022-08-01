1166cf7710b391c09a81ecea535ab7b539edb91e
KoviChen <kovi.chen@ifreegroup.com>
2022-08-01 18:30:30 +0800

    done

---
+++ src/main/java/com/kovizone/leetcode/Solution0046Permutations.java
@@ -0,0 +1,42 @@
+package com.kovizone.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/permutations/">46. 全排列</a>
+ *
+ * @author KV
+ * @since 2022/07/29
+ */
+public class Solution0046Permutations {
+
+    static int NUM = 0;
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> nodeList = new ArrayList<>();
+        permute(nodeList, new ArrayList<>(), nums);
+        return nodeList;
+    }
+
+    public void permute(List<List<Integer>> nodeList, List<Integer> prefix, int[] nums) {
+        if (nums.length == prefix.size()) {
+            nodeList.add(prefix);
+        } else {
+            for (int num : nums) {
+                if (!prefix.contains(num)) {
+                    List<Integer> newPrefix = new ArrayList<>(prefix);
+                    newPrefix.add(num);
+                    permute(nodeList, newPrefix, nums);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> permute = new Solution0046Permutations().permute(new int[]{1, 2, 3, 4});
+        for (List<Integer> integerList : permute) {
+            System.out.println(integerList);
+        }
+    }
+}

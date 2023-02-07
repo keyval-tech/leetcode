541f1271d5e15bfc18266324e0270e6b03699c07
KoviChen <kovi.chen@ifreegroup.com>
2023-02-07 11:24:11 +0800

    done

---
+++ src/main/java/com/kovizone/leetcode/page02/Solution0202.java
@@ -0,0 +1,41 @@
+package com.kovizone.leetcode.page02;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a href="https://leetcode.cn/problems/happy-number/">202. 快乐数</a>
+ *
+ * @author KV
+ * @since 2023/02/07
+ */
+public class Solution0202 {
+
+    private final static Set<Integer> CYCLE = new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    public static int next(int n) {
+        int s = 0;
+        while (n > 0) {
+            int x = n % 10;
+            s += x * x;
+            n /= 10;
+        }
+        return s;
+    }
+
+    public boolean isHappy(int n) {
+        int m = n;
+        while (m != 1 && !CYCLE.contains(m)) {
+            m = next(m);
+        }
+        if (m != 1) {
+            CYCLE.add(n);
+        }
+        return m == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution0202().isHappy(22));
+    }
+}

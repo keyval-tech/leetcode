04acc7c54c38010e2d9c9fcd89b15dbc9a45a1f6
KoviChen <kovi.chen@ifreegroup.com>
2023-05-08 17:57:12 +0800

    done

---
+++ src/main/java/com/kovizone/leetcode/page00/Solution0027.java
@@ -0,0 +1,31 @@
+package com.kovizone.leetcode.page00;
+
+import cn.hutool.core.util.ArrayUtil;
+
+/**
+ * <a href="https://leetcode.cn/problems/remove-element/?envType=study-plan-v2">27. 移除元素</a>
+ *
+ * @author KV
+ * @since 2023/05/08
+ */
+public class Solution0027 {
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            if (nums[l] == val) {
+                nums[l] = nums[r];
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 3, 5, 6};
+        System.out.println(new Solution0027().removeElement(nums, 3));
+        System.out.println(ArrayUtil.toString(nums));
+    }
+}
--- src/main/java/com/kovizone/leetcode/page00/Solution0088.java
+++ src/main/java/com/kovizone/leetcode/page00/Solution0088.java
@@ -1,5 +1,7 @@
 package com.kovizone.leetcode.page00;
 
+import cn.hutool.core.util.ArrayUtil;
+
 /**
  * <a href="https://leetcode.cn/problems/merge-sorted-array/">88. 合并两个有序数组</a>
  *
@@ -13,8 +15,16 @@ public class Solution0088 {
         int p = nums1.length - 1;
 
         while (p1 >= 0 && p2 >= 0) {
+            System.out.println(ArrayUtil.toString(nums1) + ", p1: " + p1 + ", p2: " + p2);
             nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
         }
+        System.out.println(ArrayUtil.toString(nums1) + ", p1: " + p1 + ", p2: " + p2);
         System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{3, 4, 5, 0, 0, 0};
+        int[] nums2 = new int[]{1, 2, 3};
+        new Solution0088().merge(nums1, 3, nums2, 3);
     }
 }

3606674e6ef0a2369ec3ccc8e3d6f53d90fb6d79
KoviChen <kovi.chen@ifreegroup.com>
2023-05-11 17:51:10 +0800

    done

--- src/main/java/com/kovizone/leetcode/solution/Solution0238M.java
+++ src/main/java/com/kovizone/leetcode/solution/Solution0238M.java
@@ -1,5 +1,7 @@
 package com.kovizone.leetcode.solution;
 
+import cn.hutool.core.util.ArrayUtil;
+
 /**
  * <a href="https://leetcode.cn/problems/product-of-array-except-self/">238. 除自身以外数组的乘积</a>
  * <p>
@@ -10,6 +12,30 @@ package com.kovizone.leetcode.solution;
  */
 public class Solution0238M {
     public int[] productExceptSelf(int[] nums) {
-        return null;
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = nums[i - 1] * res[i - 1];
+        }
+        int temp = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            temp = nums[i + 1] * temp;
+            res[i] *= temp;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ArrayUtil.toString(new Solution0238M().productExceptSelf(new int[]{1, 2, 3, 4})));
+
+        // 01 02 03 04
+
+        // 01 01 02 06
+        // 24 12 04 01
+
+        //          06
+
+        // 24 12 08 06
     }
 }

e9b9ead4ec2b57a75c3857dd37978dc92ac8b70d
KoviChen <kovi.chen@ifreegroup.com>
2023-06-06 15:40:06 +0800

    done

---
+++ src/main/java/com/kovizone/leetcode/solution/Solution0226E.java
@@ -0,0 +1,17 @@
+package com.kovizone.leetcode.solution;
+
+import com.kovizone.leetcode.core.TreeNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/invert-binary-tree/">226. 翻转二叉树</a>
+ * <p>
+ * #树 #深度优先搜索 #广度优先搜索 #二叉树 #简单 #面试经典150题
+ *
+ * @author KV
+ * @since 2023/06/06
+ */
+public class Solution0226E {
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
+}
